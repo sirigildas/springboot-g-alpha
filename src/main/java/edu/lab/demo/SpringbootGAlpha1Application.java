5e8248ba8c53387422a4a769564eb0b513f08d8b
Gildas Siri <gildassiri@gmail.com>
2020-12-04 02:08:57 -0500

    Initial release

---
+++ src/main/java/edu/lab/demo/SpringbootGAlpha1Application.java
@@ -0,0 +1,13 @@
+package edu.lab.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGAlpha1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGAlpha1Application.class, args);
+	}
+
+}
